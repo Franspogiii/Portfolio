c332a15e80cfa376a69241a9621445d2eb62feac
Franspogiii <jinayonjhonfrancis@gmail.com>
2025-12-16 14:08:09 +0800

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Jhon Francis Jinayon </span> Aspiring Technologist and Builder. A BSIT Bachelor of Science in Information Technologystudent driven by a profound passion for building and executing projects. Your academic journey is fueled by a hands-on approach, prioritizing the practical application of theoretical knowledge to create tangible solutions. <br><br>"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
